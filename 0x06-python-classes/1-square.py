75097e6add6773bd461eb483fdd7b4eaeff49c95
Kai7771 <uzorc2@gmail.com>
2022-12-21 16:46:54 -0800

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define aclass square"""
+
+class Square:
+
+    """Represent a square."""
+
+    def _int_(self, size):
+
+        """initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self._size = size
